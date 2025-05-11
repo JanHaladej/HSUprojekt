12bfdf875c486d182a2314b2acd1d08013540914
ABS\fikesk <fikesk@infoconsulting.eu>
2025-05-11 09:13:23 +0200

    Added parametrized training with multiple param iterations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from torchvision import transforms
+from itertools import product
 
 #Moje importy
 from Classification.DumpCNN import DumpCNN
@@ -15,45 +16,46 @@ if __name__ == '__main__':
     #data_load_class.normalizeData()
     #data_load_class.exportDataAsImages('Red', 'Green', 'Blue')
     
+
     image_pix = 256
-    
     transform = transforms.Compose([
-        #cierno biele
-        #transforms.Grayscale(num_output_channels=1),
         transforms.Resize((image_pix, image_pix)),
         transforms.ToTensor(),
-        transforms.Normalize([0.5]*3, [0.5]*3)
-        #cierno biele
-        #transforms.Normalize(mean=[0.5], std=[0.5]),
+        transforms.Normalize([0.5]*3, [0.5]*3),
     ])
-    
-    dump_dataset = DumpDataset(image_dir=data_storage_path, transform=transform)
-    
-    model_params = {
-        "input_shape": (3, image_pix, image_pix),
-        "num_classes": 2,
-        "dropout_rate": 0.45,
-        "num_conv_layers": 3,
-        "hidden_dim": 256,
-        "use_dropout": True,
-        "use_pool": True
-    }
-    
-    training_params = {
-        "epochs": 100,
-        "train_split": 0.8,
-        "batch_size": 32,
-        "learning_rate": 0.0005,
-        "patience": 10
-    }
-    
-    print("vytvaram trainer")
-    trainer = ModelTrainer(
-        dataset=dump_dataset, 
-        model_class=DumpCNN, 
-        model_params=model_params, 
-        training_params=training_params
-    )
-    
-    print("zacinam trenovat")
-    trainer.train()
+
+    dump_dataset = DumpDataset(image_dir="ImageStorage", transform=transform)
+
+    dropout_rates = [0.4]
+    num_conv_layers = [4]
+    hidden_dims = [256]
+    learning_rates = [0.0005]
+    use_pools = [True]
+
+    for dr, ncl, hd, lr, up in product(dropout_rates, num_conv_layers, hidden_dims, learning_rates, use_pools):
+        model_params = {
+            "input_shape": (3, image_pix, image_pix),
+            "num_classes": 2,
+            "dropout_rate": dr,
+            "num_conv_layers": ncl,
+            "hidden_dim": hd,
+            "use_dropout": True,
+            "use_pool": up
+        }
+
+        training_params = {
+            "epochs": 100,
+            "train_split": 0.7,
+            "batch_size": 32,
+            "learning_rate": lr,
+            "patience": 10
+        }
+
+        print(f"Training with: dr={dr}, conv={ncl}, hd={hd}, lr={lr}, pool={up}")
+        trainer = ModelTrainer(
+            dataset=dump_dataset,
+            model_class=DumpCNN,
+            model_params=model_params,
+            training_params=training_params
+        )
+        trainer.train()
